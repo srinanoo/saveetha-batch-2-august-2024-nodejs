5d4379d3b8745c73c623f04ae9575d9d37a73659
srinanoo <srinanoo@gmail.com>
2025-01-19 21:05:25 +0530

    19-01-2025

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 4000;
 
 app.use(express.urlencoded({extended: true}));
@@ -19,6 +21,35 @@ const BlogRoutes = require("./routes/blogRoutes");
 app.use("/api/v1/blog/", BlogRoutes);
 // app.use("/api/v1/author/");
 // app.use("/api/v1/categories/");
+
+app.post("/api/v1/login", (req, res) => {
+    try {
+        const { username, password, age } = req.body;
+        if(username === "Dinesh" && password === "123456") {
+            const token = jwt.sign({"username": username, "age": age}, process.env.JWT_SECRET)
+            res.json({"message": "User is valid", "token": token});
+        } else {
+            res.json({"message": "Invalid username or password"});
+        }
+    } catch (err) {
+        res.json({"error": err.message});
+    }
+});
+
+app.post("/api/v1/profile", (req, res) => {
+    try {
+        let token = req.headers?.authorization;
+
+        token = token.split(" ")[1];
+        if(!token) res.json({"message": "Token Not Found!"});
+        const data = jwt.verify(token, process.env.JWT_SECRET);
+
+        console.log(data);
+        res.json({"message": "User is valid"});
+    } catch (err) {
+        res.json({"error": err.message});
+    }
+});
 
 app.use("/*", (req, res) => {
     res.send("No routes found!");
